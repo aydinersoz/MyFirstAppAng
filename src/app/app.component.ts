baf277739c4e9b08251db41644049f4d64b86785
Your Nameaydinersoz <aydinersoz@gmail.com>
2024-01-03 19:50:07 +0300

    Değer Ekleme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,9 @@ import { RouterModule } from '@angular/router';
   template: `
   <main>
       <header class="brand-name"> 
-        <a href=".">
-        <img class="brandlogo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+        <a href="."> 
+        <img class="brandlogo" src="/assets/logo.svg" alt="logo" aria-hidden="true"><br>
+        Ana Sayfa
         </a>
       </header>
       <section class="content">
